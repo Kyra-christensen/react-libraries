29cba6bda734185719cc752b2cc2b90d4163a5b0
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-21 14:46:27 -0800

    created a DataGrid component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import DataGrid from 'react-data-grid';
 
+const columns = [
+  { key: 'id', name: 'ID' },
+  { key: 'title', name: 'Title' }
+];
+
+const rows = [
+  { id: 0, title: 'Example' },
+  { id: 1, title: 'Demo' }
+];
+
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+    <>
+      <DataGrid columns={columns} rows={rows} />;
+    </>
   );
 }
 
