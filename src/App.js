fcf49bb0ed94da26e58296d810a3c8cbc3cc6fbc
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-21 15:28:34 -0800

    installed and imported victory and created a victoryBar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import DataGrid from 'react-data-grid';
 import data from './data';
 import { generateColumns } from './data-utils';
+import { VictoryBar } from 'victory';
 
 function App() {
   return (
     <>
       <DataGrid columns= {generateColumns(data)} rows={data} />;
+
+      <VictoryBar />
     </>
   );
 }
